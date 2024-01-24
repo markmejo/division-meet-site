9a3cf698e1e4234e7f01989100b5131bb1023151
mlmejo <mlmejo12@gmail.com>
2024-01-24 15:36:34 +0800

    Create resource for municipalities

---
+++ app/Filament/Resources/MunicipalityResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MunicipalityResource\Pages;
+use App\Filament\Resources\MunicipalityResource\RelationManagers;
+use App\Models\Municipality;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MunicipalityResource extends Resource
+{
+    protected static ?string $model = Municipality::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-library';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->defaultSort('name')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMunicipalities::route('/'),
+            'create' => Pages\CreateMunicipality::route('/create'),
+            'edit' => Pages\EditMunicipality::route('/{record}/edit'),
+        ];
+    }
+}
