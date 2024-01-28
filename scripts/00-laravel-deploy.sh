3a46471066070af0b24f8e41567aa60c238ef8b9
mlmejo <mlmejo12@gmail.com>
2024-01-28 12:10:48 +0800

    Add deployment config

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Running npm"
+npm run build
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
